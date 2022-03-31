a209dd49596364bf7467e145adef7e14ec909811
CristianTura <cristianbuenaventura15@gmail.com>
2022-03-31 12:36:34 -0500

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+/*
+    Rutas de usuarios / Auth
+    host + /api/auth
+*/
+
+const { Router } = require('express')
+const { check } = require('express-validator')
+const router = Router();
+
+
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+router.post(
+    '/new',
+     [ // middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+     ], 
+     crearUsuario );
+
+router.post('/', loginUsuario );
+
+router.get('/renew', revalidarToken );
+
+module.exports = router;
